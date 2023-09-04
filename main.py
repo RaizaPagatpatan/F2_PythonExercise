4ea16ca9b49055e13916ef958bb54120a9daf21d
Raiza Pagatpatan <rjimenezpagatpatan@gmail.com>
2023-09-04 15:08:53 +0800

    3.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#
+#Number 1
 # celsius = float(input("Enter temperature in Celsius: "))
 #
 # # Convert C to F
@@ -14,16 +14,44 @@
 # print(f"The temperature in Fahrenheit is: {fahrenheit:.2f}")
 
 #Number 2
-num1 = int(input("Enter the first integer: "))
-num2 = int(input("Enter the second integer: "))
-num3 = int(input("Enter the third integer: "))
+# num1 = int(input("Enter the first integer: "))
+# num2 = int(input("Enter the second integer: "))
+# num3 = int(input("Enter the third integer: "))
+#
+# maximum = num1
+#
+# if num2 > maximum:
+#     maximum = num2
+#
+# if num3 > maximum:
+#     maximum = num3
+#
+# print(f"The maximum among {num1}, {num2}, and {num3} is: {maximum}")
 
-maximum = num1
+#Number 3
 
-if num2 > maximum:
-    maximum = num2
+num = int(input("Please enter a number: "))
 
-if num3 > maximum:
-    maximum = num3
+count = 0
+smallest = 9
+highest = 0
 
-print(f"The maximum among {num1}, {num2}, and {num3} is: {maximum}")
+
+if num == 0:
+    count = 1
+    smallest = 0
+    highest = 0
+
+while num != 0:
+    digit = num % 10
+    count += 1
+
+    smallest = min(smallest, digit)
+    highest = max(highest, digit)
+
+    num //= 10
+
+
+print(f"Number of digits in the given number is: {count}")
+print(f"Smallest number is: {smallest}")
+print(f"Highest number is: {highest}")
